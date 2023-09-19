3d422ef5fbdf3f922d10c0d2c906bb9720a8b665
Zeenath Zahra <96167605+ZeenathZahra@users.noreply.github.com>
2023-09-19 12:00:27 +0000

    copilot Third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,12 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var Comment = require('../models/comment');
-var User = require('../models/user');
-
-// Body parsing middleware
-app.use(bodyParser.json());
-
-// GET /comments
-// get all comments
-app.get('/comments', function(req, res, next) {
-    Comment.find(function(err, comments) {
-        if (err) {
-            return next(err);
-        }
-        res.json(comments);
-    });
-});
-
-// POST /comments
-// save comment
-app.post('/comments', function(req, res, next) {
-    var comment = new Comment({
-        body: req.body.body,
-    });
-
-    comment.save(function(err, comment) {
-        if (err) {
-            return next(err);
-        }
-        res.status(201).json(comment);
-    });
-});
-
-// Start the server
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log('Server is listening on port ' + port);
-});
+//Create web server
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
+const User = require('./models/user');
+const Post = require('./models/post');
+const { response } = require('express');
+const { request } = requir
